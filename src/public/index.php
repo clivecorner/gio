47e7842a3075178f03f33cabe9c6016371434405
Clive Corner <clivecorner@btinternet.com>
2024-01-06 18:47:48 +0000

    Lesson 2.2 - Classes & Objects

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,26 @@
 <?php
 
-phpinfo();
+//Classes and Objects
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+require_once 'Transaction.php';
+
+$transaction = (new Transaction(25,'25 quid'))
+  ->applyDiscount(2)
+  ->addTax(10);
+
+//var_dump($transaction->getAmount());
+
+$str = '{"a":1,"b":2,"c":3}';
+
+$arr = json_decode($str);
+
+var_dump($arr->c);
+
+$obj = new \stdClass();
+
+$obj->a = 1;
+$obj->b = 2;
+
+var_dump($obj);
+
+
