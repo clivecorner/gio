79af1cffca4f5b3dbe5b540b132541092a81537a
Clive Corner <clivecorner@btinternet.com>
2024-01-09 12:06:43 +0000

    Lesson 2.5  - spl_autoload_register

--- src/public/index.php
+++ src/public/index.php
@@ -3,11 +3,25 @@
 //NB the question remains why we have to require or include a file, and when this file includes a class import this class???
 
 //Classes and Objects
-
+/*
 require_once(__DIR__ .'/../app/PaymentGateway/Paddle/Transaction.php');
 require_once(__DIR__ .'/../app/PaymentGateway/Stripe/Transaction.php');
 require_once(__DIR__ .'/../app/PaymentGateway/Paddle/CustomerProfile.php');
 require_once(__DIR__. '/../app/Notification/Email.php');
+*/
+
+spl_autoload_register(
+  function($class){
+
+    $path = __DIR__ . '/../' . lcfirst(str_replace('\\', '/',$class). '.php');
+    echo $class;
+    echo $path;
+    require $path;
+
+  }
+);
+
+
 
 //Import class using namespace
 use App\PaymentGateway\Paddle\Transaction as PaddleTransaction;
