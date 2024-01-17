77eea14a1ed06a1623aec6acd1ce5d29b6fdf1e1
Clive Corner <clivecorner@btinternet.com>
2024-01-17 13:54:38 +0000

    Lesson 2.16 PHP variable storage and object comparison

--- src/public/index.php
+++ src/public/index.php
@@ -1,13 +1,20 @@
 <?php
-//Lesson 2.15 Anonymous classes
+//Lesson 2.16 - PHP variable storage and Object comparison 
 
 
 require __DIR__ . '/../vendor/autoload.php';
 
-//An anonymous class - Anonymous classes are principally used in testing
-$obj = new class (1, 2, 3) {
-    public function __construct(public int $i, public int $j, public int $k){
+use App\Invoice;
 
 
-    }
- };
+$invoice1 = new Invoice(25, 'My invoice 1');
+$invoice2 = new Invoice(100, 'My invoice 2');
+
+//For the below to be true all that is required is that the object is the same class and the values of properties are the same
+echo 'Invoice 1 == Invoice 2'. PHP_EOL;
+var_dump($invoice1 == $invoice2);
+
+
+//For the below to be true, which it is not, $invoice1 and $invoice2 would have to pointing to the same place in the memory
+echo 'Invoice 1 === Invoice 2' . PHP_EOL;
+var_dump($invoice1 === $invoice2);
