13355fc570c1915e09a66672e9402a94ebd24196
Clive Corner <clivecorner@btinternet.com>
2024-01-17 13:33:41 +0000

    Lesson 2.15 Anonymous classes

--- src/public/index.php
+++ src/public/index.php
@@ -1,25 +1,13 @@
 <?php
-//Lesson 2.14  Traits
-/*
-This lesson considers the fact that php doesn't permit multiple inheritance and addresses this by using Traits
-IN this context need to remember that we use the word extends for classes and implements for interfaces
+//Lesson 2.15 Anonymous classes
 
-Traits can be overwritten in the class which use the trait.
-Trait methods have precedence over extended classes
 
-NB Trait lesson also covers the following - the lesson examples the below have not been coded.
-Conflict resolution in traits - traits may contain methods with the same names
-Changing the visibility of a traits (public -> private)
-Composing traits from other Traits
-Properties in traits and accessing them in classes
-Abstract methods in traits NB the whole trait does not need to be abstract if we have an abstract methods
-Static properties and methods in Traits
-*/
 require __DIR__ . '/../vendor/autoload.php';
 
+//An anonymous class - Anonymous classes are principally used in testing
+$obj = new class (1, 2, 3) {
+    public function __construct(public int $i, public int $j, public int $k){
 
-$allInOneCoffeeMaker = new App\AllInOneCoffeeMaker();
 
-$allInOneCoffeeMaker->makeCappuccino();
-$allInOneCoffeeMaker->MakeCoffee();
-$allInOneCoffeeMaker->makeLatte();
+    }
+ };
