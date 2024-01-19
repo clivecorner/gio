5e4ac62b17352cc4fcca525b8b1b5de5f5ed49e6
Clive Corner <clivecorner@btinternet.com>
2024-01-19 10:04:29 +0000

    Lesson 2.21 The Date Time object

--- src/public/index.php
+++ src/public/index.php
@@ -1,71 +1,37 @@
 <?php
-//Lesson 2.20 PHP exceptions
-
+//Lesson 2.21 Date and Time object
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Invoice;
-use App\Customer;
+//echo strtotime('tomorrow');
 
-/*
-
-//Errors in procedural PHP  see https://www.php.net/manual/en/errorfunc.constants.php
-//error_reporting(E_ALL & ~E_WARNING);
-
-//When triggering errors can only use E_USER errors see above link
-//trigger_error('Example error', E_USER_ERROR);
-
-function errorHandler(
-  int $type,
-  string $message,
-  ?string $file = null,
-  ?int $line = null
-)
-{
-  echo $type . ':' . $message . ':' . $file . ':' . $line;
-}
-
-set_error_handler('errorHandler', E_ALL);
-
-echo $x;
-
-*/
-
-/*
-
-//What is an exception? An exception is an object of some exception class that describes an error. It disrupts
-//the flow of the codes execution.
-
-$customer = new Customer;
-$invoice = new Invoice($customer);
-try {
-  $invoice->process(77);
-} catch(App\MissingBillingException $e) {
-  echo $e->getMessage() . ':' . $e->getLine() . ':' . $e->getFile();
-} catch(\InvalidArgumentException){
-  echo 'Invalid argument';
-} finally {
-  echo 'Something you want to run despite the exceptions';
-}
+// DateTime takes the same arguments as strtotime
+//var_dump(new \DateTime('tomorrow'));
 
 
-*/
+//var_dump(new \DateTime('tomorrow', new DateTimeZone('Europe/London')));
 
 
+//$dateTime = new \DateTime('tomorrow', new DateTimeZone('Europe/London'));
 
-//For global exception handling -- NB we still need to throw an exception --- this is a catch all!!!
-set_exception_handler(function (\Throwable $e) {
-  echo $e->getMessage() . ' NB from message handler';
-});
+//echo $dateTime->format('m/d/Y g:i A');
 
-
-$customer = new Customer;
-$invoice = new Invoice($customer);
-
-$invoice->process(-45);
-
-//Yet another approach is to use an exception class which is specific to domain, in this example InvoiceException
-// In this we create a series of static methods which we throw.
+//See https://www.php.net/manual/en/book.datetime.php for datetime docs 
 
 
+//Using the datetime static method create from format 
 
+//European date format
+$date = '15/05/24 3:40PM';
+$date2 = '20/08/21 4:00AM';
+
+$dateTime = DateTime::createFromFormat('d/m/Y g:iA',$date);
+$dateTime2 = DateTime::createFromFormat('d/m/Y g:iA',$date2);
+
+echo $dateTime->diff($dateTime2)->days;
+
+//As well as calculating the difference between dates Gio also covers:
+//Adding and subtracting dates
+//Problem with date time object and how to solve it
+//Immutable date time objects
+//Iterating over dates in a period
