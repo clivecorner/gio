96d85ffca6244373542ed098335a6858869558a1
Clive Corner <clivecorner@btinternet.com>
2024-01-19 12:02:23 +0000

    //Lesson 2.22 Iterators and iterable type

--- src/public/index.php
+++ src/public/index.php
@@ -1,37 +1,22 @@
 <?php
-//Lesson 2.21 Date and Time object
+//Lesson 2.22 Iterators and iterable type
 
 require __DIR__ . '/../vendor/autoload.php';
 
-//echo strtotime('tomorrow');
+$invoice = new App\Invoice(25);
 
-// DateTime takes the same arguments as strtotime
-//var_dump(new \DateTime('tomorrow'));
-
-
-//var_dump(new \DateTime('tomorrow', new DateTimeZone('Europe/London')));
-
-
-//$dateTime = new \DateTime('tomorrow', new DateTimeZone('Europe/London'));
-
-//echo $dateTime->format('m/d/Y g:i A');
-
-//See https://www.php.net/manual/en/book.datetime.php for datetime docs 
+//Iterating over an object
+foreach($invoice as $key => $value){
+  //echo $key . '  =  ' . $value . PHP_EOL;
+}
 
 
-//Using the datetime static method create from format 
+$invoiceCollection = new App\InvoiceCollection([new App\Invoice(25), new App\Invoice(100), new App\Invoice(40)]);
 
-//European date format
-$date = '15/05/24 3:40PM';
-$date2 = '20/08/21 4:00AM';
 
-$dateTime = DateTime::createFromFormat('d/m/Y g:iA',$date);
-$dateTime2 = DateTime::createFromFormat('d/m/Y g:iA',$date2);
+foreach($invoiceCollection as $invoice){
 
-echo $dateTime->diff($dateTime2)->days;
+  echo $invoice->id . ' = ' . $invoice->amount . PHP_EOL;
+}
 
-//As well as calculating the difference between dates Gio also covers:
-//Adding and subtracting dates
-//Problem with date time object and how to solve it
-//Immutable date time objects
-//Iterating over dates in a period
+//Also includes using the iterator Aggregates 
